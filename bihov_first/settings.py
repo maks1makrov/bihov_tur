839d9e8a30ba1d544dda376939bba909a3a64d78
Maks <maks1makrov@gmail.com>
2021-01-28 20:54:21 +0300

    add static

--- bihov_first/settings.py
+++ bihov_first/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 from os import environ
 from pathlib import Path
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bihov_first.urls'
@@ -131,7 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
--- tourism/urls.py
+++ tourism/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from tourism import views
 
 urlpatterns = [
-    path('categories', views.ListFieldAPI.as_view(), name='categories'),
+    path('categories/', views.ListFieldAPI.as_view(), name='categories'), #все категории
     path('categories/<int:id>', views.ListFieldAPI.as_view(), name='one_categories'),
-    path('toursites/', views.ListToursitesAPI.as_view(), name="toursites"),
-    path('toursites_of_category/<int:id>', views.ListToursitesAPI.as_view(), name="toursites_categories"),
-    path('toursite_number/<int:number>', views.ListToursitesAPI.as_view(), name="toursite_full"),
-    path('articles', views.ListArticlesAPI.as_view(), name="articles"),
-    path('articles/<int:id>', views.ListArticlesAPI.as_view(), name="articles_id"),
+    path('toursites/', views.ListToursitesAPI.as_view(), name="toursites"), #все туристические объекты
+    path('toursites_of_category/<int:id>/', views.ListToursitesAPI.as_view(), name="toursites_categories"),#тур объекты определенной категории
+    path('toursites/<int:number>/', views.ListToursitesAPI.as_view(), name="toursite_full"), #тур объект с конкретным id
+    path('articles/', views.ListArticlesAPI.as_view(), name="articles"),#все статьи
+    path('articles/<int:id>/', views.ListArticlesAPI.as_view(), name="articles_id"),#статья с конкретным id
     path('test/', views.TestIndex.as_view(), name="test"),
 
 ]
